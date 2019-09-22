45ccef2b4df59e8d6a955c67156f0ad3d654f289
caiyi04 <caiyi04@maoyan.com>
2019-09-22 14:08:46 +0800

    第7周

--- 101. 对称二叉树.js
+++ 101. 对称二叉树.js
@@ -1,46 +1,37 @@
 /**
- * 递归
- * @param {string} s
- * @return {string[][]}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
  */
-var partition = function(s) {
-    return partitionHelper(s, 0);
-};
-
-function partitionHelper(s, start) {
-    //递归出口
-    if (start == s.length) {
-        let list = [], res = [];
-        res.push(list);
-        console.log(res,'res',list);
-        return res;
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    let res=[];
+    inOrder(root);//中序遍历  输出所有节点 理想状态下  是回文
+    // console.log(res)
+    if(res.length%2===0&&root) return false; //如果树的节点数是偶数(排除0) 返回false;
+    
+    //用首尾双指针判断是否回文数组:
+    let i=0;
+    let j=res.length-1;
+    while(i < j){
+          //有不满足条件的组  返回false
+          if(res[i].val!=res[j].val||res[i].pos===res[j].pos) return false;    
+          i++;
+          j--;
     }
-    let res = [];
-    for (let i = start; i < s.length; i++) {
-        //当前切割后是回文串才考虑
-        if (isPalindrome(s.slice(start, i + 1))) {
-            let left = s.slice(start, i + 1);
-            //遍历后边字符串的所有结果，将当前的字符串加到头部
-            for (let list of partitionHelper(s, i + 1)) {
-                console.log(list,left,'------list------left--------------------')
-                list.unshift(left);
-                res.push(list);
-            }
-        }
-    }
-    return res;
-}
-function isPalindrome(s) {
-    let i = 0;
-    let j = s.length - 1;
-    while (i < j) {
-        if (s[i] != s[j]) {
-            return false;
-        }
-        i++;
-        j--;
-    }
+    //没有不满足的情况  返回true
     return true;
-}
-var s = "abb";
-partition(s)
+    //中序遍历: 对称树必定会输出一个回文数组,再加上节点的位置标记,可以过滤掉形如示例二的树
+    function inOrder(root,pos=NaN){
+        if(root!=null){
+        inOrder(root.left,'l')
+        res.push({val:root.val,pos});console.log(root.val);
+        inOrder(root.right,'r')
+      }
+    }
+};

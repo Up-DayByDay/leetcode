af8c4ad5e8a724cbfa0a2b1c2f7af6bbc4773e36
caiyi04 <caiyi04@maoyan.com>
2019-09-23 11:55:23 +0800

    第7周

--- 101. 对称二叉树.js
+++ 101. 对称二叉树.js
@@ -4,34 +4,50 @@
  *     this.val = val;
  *     this.left = this.right = null;
  * }
+ * 递归和迭代两种方法解决这个问题，会很加分。
  */
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isSymmetric = function(root) {
-    let res=[];
-    inOrder(root);//中序遍历  输出所有节点 理想状态下  是回文
-    // console.log(res)
-    if(res.length%2===0&&root) return false; //如果树的节点数是偶数(排除0) 返回false;
+//迭代
+    // var isSymmetric = function(root) {
+    // let res=[];
+    // inOrder(root);
+    // if(res.length%2===0&&root) return false; //如果树的节点数是偶数(排除0) 返回false;
     
-    //用首尾双指针判断是否回文数组:
-    let i=0;
-    let j=res.length-1;
-    while(i < j){
-          //有不满足条件的组  返回false
-          if(res[i].val!=res[j].val||res[i].pos===res[j].pos) return false;    
-          i++;
-          j--;
-    }
-    //没有不满足的情况  返回true
-    return true;
-    //中序遍历: 对称树必定会输出一个回文数组,再加上节点的位置标记,可以过滤掉形如示例二的树
-    function inOrder(root,pos=NaN){
-        if(root!=null){
-        inOrder(root.left,'l')
-        res.push({val:root.val,pos});console.log(root.val);
-        inOrder(root.right,'r')
-      }
-    }
+    // //用首尾双指针判断是否回文数组:
+    // let i=0;
+    // let j=res.length-1;
+    // while(i < j){
+    //       if(res[i].val!=res[j].val||res[i].pos===res[j].pos) return false;    
+    //       i++;
+    //       j--;
+    // }
+    // return true;
+    // //中序遍历: 对称树必定会输出一个回文数组,再加上节点的位置标记
+    // function inOrder(root,pos=NaN){
+    //     if(root!=null){
+    //     inOrder(root.left,'l')
+    //     res.push({val:root.val,pos});
+    //     inOrder(root.right,'r')
+    //   }
+    // }
+    // };
+//递归
+var isSymmetric = function(root) {
+    return isMirror(root, root);
+}
+
+function isMirror(t1, t2) {
+    if (t1 == null && t2 == null) return true;
+    if (t1 == null || t2 == null) return false;
+    return (t1.val == t2.val)
+        && isMirror(t1.right, t2.left)
+        && isMirror(t1.left, t2.right);
 };
+
+
+
+
+
--- 73. 矩阵置零.js
+++ 73. 矩阵置零.js
@@ -3,37 +3,104 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  * O(1)空间的暴力
+ * 方法1：MODIFIED 不太严谨
+ * 方法2：标记 行列第一个元素
+ * 方法3：0 === -0 漏洞=》标记 -0
  */
 var setZeroes = function(matrix) {
-    const MODIFIED = -1000000;
-    const R = matrix.length;
-    const C = matrix[0].length;
+    // const MODIFIED = -1000000;
+    // const R = matrix.length;
+    // const C = matrix[0].length;
 
-    for (let r = 0; r < R; r++) {
-      for (let c = 0; c < C; c++) {
-        if (matrix[r][c] == 0) {
-          // We modify the corresponding rows and column elements in place.
-          // Note, we only change the non zeroes to MODIFIED
-          for (let k = 0; k < C; k++) {
-            if (matrix[r][k] != 0) {
-              matrix[r][k] = MODIFIED;
-            }
-          }
-          for (let k = 0; k < R; k++) {
-            if (matrix[k][c] != 0) {
-              matrix[k][c] = MODIFIED;
-            }
-          }
+    // for (let r = 0; r < R; r++) {
+    //   for (let c = 0; c < C; c++) {
+    //     if (matrix[r][c] == 0) {
+    //       for (let k = 0; k < C; k++) {
+    //         if (matrix[r][k] != 0) {
+    //           matrix[r][k] = MODIFIED;
+    //         }
+    //       }
+    //       for (let k = 0; k < R; k++) {
+    //         if (matrix[k][c] != 0) {
+    //           matrix[k][c] = MODIFIED;
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+
+    // for (let r = 0; r < R; r++) {
+    //   for (let c = 0; c < C; c++) {
+    //     if (matrix[r][c] == MODIFIED) {
+    //       matrix[r][c] = 0;
+    //     }
+    //   }
+    // }
+
+    let isCol = false;
+    let R = matrix.length;
+    let C = matrix[0].length;
+
+    for (let i = 0; i < R; i++) {
+      if (matrix[i][0] == 0) {
+        isCol = true;
+      }
+
+      for (let j = 1; j < C; j++) {
+        if (matrix[i][j] == 0) {
+          matrix[0][j] = 0;
+          matrix[i][0] = 0;
+        }
+      }
+    }
+    for (let i = 1; i < R; i++) {
+      for (let j = 1; j < C; j++) {
+        //如果被标记的为0，则此项为0
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
         }
       }
     }
 
-    for (let r = 0; r < R; r++) {
-      for (let c = 0; c < C; c++) {
-        // Make a second pass and change all MODIFIED elements to 0 """
-        if (matrix[r][c] == MODIFIED) {
-          matrix[r][c] = 0;
-        }
+    if (matrix[0][0] == 0) {
+      for (let j = 0; j < C; j++) {
+        matrix[0][j] = 0;
       }
     }
+    if (isCol) {
+      for (let i = 0; i < R; i++) {
+        matrix[i][0] = 0;
+      }
+    }
+
+    /**
+     * 标记 -0 有些作弊
+     */
+    // let i, j;
+    // i = matrix.length;
+    // while(i--){
+    //     j = matrix[i].length;
+    //     while(j--){
+    //         if(checkIsZero(i, j)){
+    //             setZero(i, j);      
+    //         }
+    //     }
+    // }
+    
+    // function checkIsZero(i, j){
+    //     return !matrix[i][j] && 1 / matrix[i][j] > 0;
+    // }
+    // function setZero(i, j){
+    //     let k;
+    //     k = 0;
+    //     while(k < matrix.length){
+    //         matrix[k][j] = matrix[k][j] ? -0 : matrix[k][j];
+    //         k++;
+    //     }
+    //     k = 0;
+    //     while(k < matrix[i].length){
+    //         matrix[i][k] = matrix[i][k] ? -0 : matrix[i][k];
+    //         k++;
+    //     }
+    // }
 };

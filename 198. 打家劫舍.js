d0a32bea35296fda20aae2683b780ffee6d3361d
caiyi04 <caiyi04@maoyan.com>
2019-10-06 22:18:25 +0800

    第9周

--- 198. 打家劫舍.js
+++ 198. 打家劫舍.js
@@ -7,7 +7,8 @@ var rob = function(nums) {
     if(nums.length === 1)return nums[0];
     if(nums.length === 2)return Math.max(...nums);
     
-    // 动态规划 方程 f(i) = Math.max(f(i-1), f(i-1)+nums[i])
+    // 动态规划 方程 f(i) = Math.max(f(i-1), f(i-2)+nums[i])
+    // f(k) = 从前 k 个房屋中能抢劫到的最大数额， nums[i] = 第 i 个房屋的钱数。
     let prepre = nums[0];
     let pre = Math.max(nums[0], nums[1]);
     for(let i = 2; i < nums.length; i++){
